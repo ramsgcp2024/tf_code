122e1ff61825bef3ad83850820d4ca7e6763d3b3
ramsgcp2024 <ramsgcp2024@gmail.com>
2024-06-24 12:33:00 +0530

    added sonarqube

--- variables.tf
+++ variables.tf
@@ -36,6 +36,10 @@ variable "instances" {
       instance_type = "e2-medium"
       zone = "us-west4-a"
     }
+     "sonarqube" = {
+      instance_type = "e2-medium"
+      zone = "us-west4-a"
+    }
   }   
 }
 
